587ba5bc90a2d553c0de97593720458298fc9a86
nilsreich <nilsreich@gmail.com>
2022-02-17 20:55:28 +0000

    nix

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -1,21 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+const PUBLIC_FILE = /\.(.*)$/
+
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
+  const country = req.geo.country?.toLowerCase() || 'us'
+  const locale = req.headers.get('accept-language')?.split(',')?.[0] || 'en-US'
 
-  if (basicAuth) {
-    const auth = basicAuth.split(' ')[1]
-    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
-
-    if (user === '4dmin' && pwd === 'testpwd123') {
-      return NextResponse.next()
-    }
+  // Only rewrite files that don't have a file extension
+  if (!PUBLIC_FILE.test(req.nextUrl.pathname)) {
+    req.nextUrl.pathname = `/${locale}/${country}`
+    return NextResponse.rewrite(req.nextUrl)
   }
-
-  return new Response('Auth required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  })
 }

9d02055f54034b2dfbc37eab118123934d2b2419
nilsreich <nilsreich@gmail.com>
2022-02-19 14:14:52 +0000

    new

---
+++ pages/comic/index.tsx
@@ -0,0 +1,98 @@
+import Page from '@/components/page'
+
+const Index=()=>{
+
+	return(	
+		<Page>
+    <div className="relative w-full flex gap-0 snap-x overflow-x-auto">
+      <div className="fixed w-12 h-screen bg-gradient-to-r from-white"></div> 
+      <div className="fixed w-12 h-screen bg-gradient-to-l right-0 from-white"></div> 
+      <div
+      className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+    >
+      <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/01.webp" />
+    </div>
+    <div
+    className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+  >
+    <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/02.webp" />
+    <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/03.webp" />
+  </div>
+  <div
+  className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+>
+  <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/04.webp" />
+  <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/05.webp" />
+</div>
+<div
+					className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+>
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/06.webp" />
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/07.webp" />
+</div>
+<div
+					className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+>
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/08.webp" />
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/09.webp" />
+</div>
+<div
+					className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+>
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/10.webp" />
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/11.webp" />
+</div>
+<div
+					className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+>
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/12.webp" />
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/13.webp" />
+</div>
+<div
+					className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+>
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/14.webp" />
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/15.webp" />
+</div>
+<div
+					className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+>
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/16.webp" />
+<img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/17.webp" />
+</div>
+      <div
+        className="snap-normal snap-center shrink-0 last:pr-24 flex border-r border-black"
+      >
+        <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/18.webp" />
+        <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/19.webp" />
+      </div>
+
+      <div
+        className="snap-normal snap-center shrink-0 first:pl-24 last:pr-24 flex border-r border-black"
+      >
+        <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/20.webp" />
+        <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/21.webp" />
+      </div>
+
+      <div
+        className="snap-normal snap-center shrink-0 first:pl-24 last:pr-24 flex border-r border-black"
+      >
+        <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/22.webp" />
+        <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/23.webp" />
+      </div>
+      <div
+        className="snap-normal snap-center shrink-0 first:pl-24 last:pr-24 flex border-r border-black"
+      >
+        <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/24.webp" />
+        <img className="shrink-0 h-screen aspect-[2/3] object-fit" src="./images/25.webp" />
+      </div>
+
+      <div className="snap-center shrink-0">
+        <div className="shrink-0 w-4 sm:w-48"></div>
+      </div>
+    </div>
+	</Page>
+	)
+}
+
+export default Index
